f0d312f52b647a1733bb391b7ee357687a9a1c8a
Lok <nail_228337@mail.ru>
2026-06-23 20:31:10 +0500

    Доработка возможности установить причину изменения статуса + обновление документации API

--- app/Models/PracticeRequest.php
+++ app/Models/PracticeRequest.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-#[Fillable(['user_id', 'status_id', 'name', 'surname', 'patronymic', 'specialization', 'course', 'start_date', 'end_date'])]
+#[Fillable(['user_id', 'status_id', 'status_change_reason', 'name', 'surname', 'patronymic', 'specialization', 'course', 'start_date', 'end_date'])]
 #[Hidden(['user_id', 'status_id'])]
 class PracticeRequest extends Model
 {
